227d45c58a9414307c4a547a9bcec7708540ef11
MajorX234 <majorx234@googlemail.com>
2023-11-16 00:36:04 +0100

    Add close button, move jack thread responsibility into gui

--- src/midi_debugger/main.rs
+++ src/midi_debugger/main.rs
@@ -1,6 +1,6 @@
 use crossbeam_channel::unbounded;
 use eframe;
-use midi_mapper::jackmidi::MidiMsg;
+use midi_mapper::{jackmidi::MidiMsg, jackprocess::start_jack_thread};
 use std::sync::mpsc;
 use wmidi;
 mod midi_debugger_gui;
@@ -11,7 +11,21 @@ fn main() {
         std::sync::mpsc::SyncSender<MidiMsg>,
         std::sync::mpsc::Receiver<MidiMsg>,
     ) = mpsc::sync_channel(64);
+    let (tx_close, rx_close) = unbounded();
+    let jack_midi_thread = start_jack_thread(rx_close, midi_sender);
     let midi_debugger_gui = MidiDebuggerGui {
         midi_receiver: Some(midi_receiver),
+        midi_thread: Some(jack_midi_thread),
+        tx_close: Some(tx_close),
     };
+
+    let mut options = eframe::NativeOptions::default();
+    let window_size: eframe::egui::Vec2 = eframe::egui::Vec2::new(525.0, 530.0);
+    options.initial_window_size = Some(window_size);
+
+    eframe::run_native(
+        "MidiDebuggerGui",
+        options,
+        Box::new(|_cc| Box::new(midi_debugger_gui)),
+    );
 }
--- src/midi_debugger/midi_debugger_gui.rs
+++ src/midi_debugger/midi_debugger_gui.rs
@@ -4,28 +4,40 @@ use midi_mapper::jackmidi::MidiMsg;
 
 pub struct MidiDebuggerGui {
     pub midi_receiver: Option<std::sync::mpsc::Receiver<MidiMsg>>,
+    pub midi_thread: Option<std::thread::JoinHandle<()>>,
+    pub tx_close: Option<crossbeam_channel::Sender<bool>>,
 }
 
 impl Default for MidiDebuggerGui {
     fn default() -> Self {
         Self {
             midi_receiver: None,
+            midi_thread: None,
+            tx_close: None,
         }
     }
 }
 
 impl eframe::App for MidiDebuggerGui {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("MidiMapper");
-        });
         if let Some(ref midi_receiver) = self.midi_receiver {
-            if let Ok(m) = midi_receiver.recv() {
+            if let Ok(m) = midi_receiver.try_recv() {
                 let bytes: &[u8] = &m.data;
                 if let Ok(message) = wmidi::MidiMessage::try_from(bytes) {
                     println!("{:?}", message);
                 }
             }
         }
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("MidiMapper");
+            ui.vertical(|ui| {
+                if ui.button("close").clicked() {
+                    if let Some(x) = &self.tx_close {
+                        x.send(false).unwrap();
+                        _frame.quit();
+                    };
+                }
+            });
+        });
     }
 }
