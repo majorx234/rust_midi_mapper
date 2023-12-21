787a26ede01c77b6afd3d7322973f50573d27b7d
MajorX234 <majorx234@googlemail.com>
2023-12-22 00:08:45 +0100

    Add executeable to test Midi visualisation grouping by id

---
+++ src/midi_elements_gui/main.rs
@@ -0,0 +1,49 @@
+/*
+ * This file is part of the rust_midi_mapper distribution (https://github.com/majorx234/rust_midi_mapper ).
+ * Copyright (c) 2023-2024 Majorx234 <majorx234@googlemail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+use crossbeam_channel::unbounded;
+use eframe;
+use midi_mapper::{jackmidi::MidiMsg, jackprocess::start_jack_thread};
+use std::sync::mpsc;
+mod midi_elements_gui;
+use midi_elements_gui::MidiElementsGui;
+
+fn main() {
+    let (midi_sender, midi_receiver): (
+        std::sync::mpsc::SyncSender<Box<dyn MidiMsg>>,
+        std::sync::mpsc::Receiver<Box<dyn MidiMsg>>,
+    ) = mpsc::sync_channel(64);
+    let (tx_close, rx_close) = unbounded();
+    let jack_midi_thread = start_jack_thread(rx_close, midi_sender);
+    let midi_elements_gui = MidiElementsGui {
+        midi_receiver: Some(midi_receiver),
+        midi_thread: Some(jack_midi_thread),
+        tx_close: Some(tx_close),
+        n_items: 0,
+        midi_msgs: Vec::new(),
+    };
+
+    let mut options = eframe::NativeOptions::default();
+    let window_size: eframe::egui::Vec2 = eframe::egui::Vec2::new(500.0, 530.0);
+    options.initial_window_size = Some(window_size);
+
+    let _ = eframe::run_native(
+        "MidiDebuggerGui",
+        options,
+        Box::new(|_cc| Box::new(midi_elements_gui)),
+    );
+}
---
+++ src/midi_elements_gui/midi_elements_gui.rs
@@ -0,0 +1,85 @@
+/*
+ * This file is part of the rust_midi_mapper distribution (https://github.com/majorx234/rust_midi_mapper ).
+ * Copyright (c) 2023-2024 Majorx234 <majorx234@googlemail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+use eframe::egui::{self, ScrollArea};
+use midi_mapper::jackmidi::MidiMsg;
+
+pub struct MidiElementsGui {
+    pub midi_receiver: Option<std::sync::mpsc::Receiver<Box<dyn MidiMsg>>>,
+    pub midi_thread: Option<std::thread::JoinHandle<()>>,
+    pub tx_close: Option<crossbeam_channel::Sender<bool>>,
+    pub n_items: usize,
+    pub midi_msgs: Vec<Box<dyn MidiMsg>>,
+}
+
+impl Default for MidiElementsGui {
+    fn default() -> Self {
+        Self {
+            midi_receiver: None,
+            midi_thread: None,
+            tx_close: None,
+            n_items: 0,
+            midi_msgs: Vec::new(),
+        }
+    }
+}
+
+impl eframe::App for MidiElementsGui {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        let mut received_midi_msgs = Vec::new();
+        if let Some(ref midi_receiver) = self.midi_receiver {
+            while let Ok(m) = midi_receiver.try_recv() {
+                received_midi_msgs.push(m);
+            }
+        }
+        let window_size = _frame.info().window_info.size;
+        let window_width = window_size[0];
+        let window_height = window_size[1];
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("MidiElementsGui");
+            ui.vertical(|ui| {
+                if ui.button("close").clicked() {
+                    if let Some(x) = &self.tx_close {
+                        x.send(false).unwrap();
+                        _frame.close();
+                    };
+                }
+
+                self.midi_msgs.append(&mut received_midi_msgs);
+                self.n_items = self.midi_msgs.len();
+                let text_style = egui::TextStyle::Body;
+                let row_height = ui.text_style_height(&text_style);
+                ScrollArea::vertical()
+                    .stick_to_bottom(true)
+                    .min_scrolled_height(window_height - 60.0)
+                    .max_height(window_height - 60.0)
+                    .min_scrolled_width(window_width - 40.0)
+                    .max_width(window_width - 40.0)
+                    .show_rows(ui, row_height, self.n_items, |ui, row_range| {
+                        for row in row_range {
+                            if row > 0 {
+                                let text = format!("{}", self.midi_msgs[row - 1]);
+                                ui.label(text);
+                            }
+                        }
+                    });
+
+                ui.ctx().request_repaint();
+            });
+        });
+    }
+}
