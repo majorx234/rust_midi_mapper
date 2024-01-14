7822f5a3137072b2444e58b4cd567935ab06b9d5
MajorX234 <majorx234@googlemail.com>
2024-01-14 23:59:00 +0100

    Add parsing function json -> MidiFunctionfile

--- src/midi_function.rs
+++ src/midi_function.rs
@@ -1,5 +1,8 @@
+use clap::error::ErrorFormatter;
 use serde::{Deserialize, Serialize};
-use std::fmt;
+use std::io::Read;
+use std::result::Result;
+use std::{collections::HashSet, error::Error, fmt, fs::File};
 
 #[derive(Hash, Eq, PartialEq, Serialize, Deserialize, Copy, Clone, Debug)]
 pub enum MidiFunction {
@@ -16,4 +19,23 @@ impl fmt::Display for MidiFunction {
         // or, alternatively:
         // fmt::Debug::fmt(self, f)
     }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct MidiFunctionFile {
+    pub midi_functions: Vec<MidiFunction>,
+}
+
+pub fn parse_json_file_to_midi_functions(file_path_str: &String) -> MidiFunctionFile {
+    let mut file_content = match File::open(file_path_str) {
+        Ok(file) => file,
+        Err(_) => panic!("Could not read the json file"),
+    };
+    let mut contents = String::new();
+    match file_content.read_to_string(&mut contents) {
+        Ok(_) => {}
+        Err(err) => panic!("Could not deserialize the file, error code: {}", err),
+    };
+    let module: MidiFunctionFile = serde_json::from_str(&contents.as_str()).unwrap();
+    module
 }
