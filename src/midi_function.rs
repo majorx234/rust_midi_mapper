374fef172b9317384da7e684b58b0cffe8a3c401
MajorX234 <majorx234@googlemail.com>
2024-01-18 23:53:32 +0100

    Simplify error passing

--- src/midi_function.rs
+++ src/midi_function.rs
@@ -31,29 +31,16 @@ pub struct MidiFunctionFile {
     pub midi_functions: Vec<MidiFunction>,
 }
 
-pub fn parse_json_file_to_midi_functions(file_path_str: &String) -> Result<MidiFunctionFile, &str> {
-    let mut file_content = match File::open(file_path_str) {
-        Ok(file) => file,
-        Err(err) => {
-            println!("Could not read the json file: {}", err);
-            return Err("Could not read the json file");
-        }
-    };
+pub fn parse_json_file_to_midi_functions(
+    file_path_str: &String,
+) -> Result<MidiFunctionFile, String> {
+    let mut file_content =
+        File::open(file_path_str).map_err(|err| format!("Could not read the json file {}", err))?;
     let mut contents = String::new();
-    match file_content.read_to_string(&mut contents) {
-        Ok(_) => {}
-        Err(err) => {
-            println!("Could not read file to string: {}", err);
-            return Err("Could not read file to string");
-        }
-    };
-    let module: MidiFunctionFile = match serde_json::from_str(contents.as_str()) {
-        Ok(module) => module,
-        Err(err) => {
-            println!("error in json desirialize: {}", err);
-            return Err("error in json desirialize");
-        }
-    };
-    println!("{:?}", module);
+    file_content
+        .read_to_string(&mut contents)
+        .map_err(|err| format!("Could not read file to string {}", err))?;
+    let module: MidiFunctionFile = serde_json::from_str(contents.as_str())
+        .map_err(|err| format!("error in json deserialize {}", err))?;
     Ok(module)
 }
